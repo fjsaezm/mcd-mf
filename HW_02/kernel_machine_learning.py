dfc56acf3977bfba8cfda8fbd0a663d131d8d14c
Jose <ocetebt@hotmail.com>
2022-02-22 13:09:44 +0100

    Working on HW_02 kernel pca, only theorical analysis remaining

--- HW_02/kernel_machine_learning.py
+++ HW_02/kernel_machine_learning.py
@@ -2,7 +2,13 @@ from typing import Callable, Tuple
 
 import numpy as np
 from scipy.spatial import distance
+
 from sklearn.preprocessing import normalize
+from sklearn.utils.extmath import svd_flip
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 
 def linear_kernel(
     X: np.ndarray,
@@ -67,6 +73,7 @@ def kernel_pca(
     X: np.ndarray,
     X_test: np.ndarray,
     kernel: Callable,
+    deterministic_orientation: bool = False
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Parameters
@@ -75,10 +82,11 @@ def kernel_pca(
         Data matrix
     X_test:
         data matrix
-    A:
-        output variance
-    ls:
-        kernel lengthscale
+    kernel:
+        Kernel function
+    deterministic_orientation:
+        whether or not to orientate the resulsts
+        as done by Sklearn
 
     Returns
     -------
@@ -111,8 +119,13 @@ def kernel_pca(
     # Obtain the SVD decomposition of the kernel matrix
     alpha_eigenvecs, lambda_eigenvals, V = np.linalg.svd(K_tilda, full_matrices=True)
     
-    # Normalize the eigenvectors
-    alpha_eigenvecs = normalize(alpha_eigenvecs, axis=0)
+    # Make the eigenvector signs match the ones obtained from Sklearn
+    if deterministic_orientation:
+        alpha_eigenvecs, _ = svd_flip(alpha_eigenvecs, V)
+    
+    # Normalize the eigenvectors so their norm equals 1/lambda_i
+    for i, lambda_i in enumerate(np.sqrt(lambda_eigenvals)):
+        alpha_eigenvecs[:,i] /= lambda_i
     
     # Compute the Gram matrix for the test observations
     K_test = kernel(X_test, X_test)
@@ -124,5 +137,130 @@ def kernel_pca(
             
     # Project the test observations to the found principal components
     X_test_hat = K_tilda_test @ alpha_eigenvecs
-
+    
     return X_test_hat, lambda_eigenvals, alpha_eigenvecs
+
+
+class AnimationKPCA:
+    
+    def __init__(
+        self,
+        xlims: Tuple[float, float] = (-1, 1),
+        ylims: Tuple[float, float] = (-1, 1),
+        n_frames: int = 50,
+    ):
+        self.xlims = xlims
+        self.ylims = ylims
+        self.n_frames = n_frames
+        self.gammas = 2*np.logspace(-3, 4, n_frames)
+        self.A = 1.0
+        self.L = 1.0
+        
+    def _init_frame_plot(
+        self,
+        ax: matplotlib.axes.Axes,
+        gamma: float,
+    ):
+        ax.clear()
+        ax.set_title(r"Projection by KPCA ($\gamma=${:.3f}".format(gamma))
+        ax.set_xlabel(r"1st principal component in space induced by $\phi$")
+        ax.set_ylabel("2nd principal component")
+        ax.set_xlim(self.xlims)
+        ax.set_ylim(self.ylims)
+    
+    def _update_animation(
+        self,
+        i_frame: int,
+        ax: matplotlib.axes.Axes,
+        X: np.ndarray,
+        X_test: np.ndarray,
+        reds: np.ndarray,
+        blues: np.ndarray,
+    ):
+        gamma = self.gammas[i_frame]
+        self.L = np.sqrt(0.5/gamma)
+        self._init_frame_plot(ax, gamma)
+
+        X_kpca, _, _ = kernel_pca(X, X_test, self.kernel,
+                                  deterministic_orientation=True)
+
+        ax.scatter(X_kpca[reds, 0], X_kpca[reds, 1], c="red",
+                   s=20, edgecolor='k')
+        ax.scatter(X_kpca[blues, 0], X_kpca[blues, 1], c="blue",
+                   s=20, edgecolor='k')
+    
+    def animate(
+        self,
+        X: np.ndarray,
+        X_test: np.ndarray,
+        y_test: np.ndarray,
+    ) -> matplotlib.animation.FuncAnimation:
+        fig = plt.figure(figsize=(10, 7))
+        ax = fig.add_subplot(111)
+
+        def kernel(X, X_prime):
+            return rbf_kernel(X, X_prime, self.A, self.L)
+
+        self.kernel = kernel
+        reds = y_test == 0
+        blues = y_test == 1
+
+        return anim.FuncAnimation(
+            fig,
+            self._update_animation,
+            frames=self.n_frames,
+            repeat=False,
+            fargs=(ax, X, X_test, reds, blues,),
+            blit = False
+        )
+    
+    
+def plot_KPCA(gamma, X, y, A=1.0, lims=None):
+    # Parameters
+    L = np.sqrt(0.5/gamma)
+    reds = y == 0
+    blues = y == 1
+
+    # Kernel definition
+    def local_rbf_kernel(X, X_prime):
+        return rbf_kernel(X, X_prime, A, L)
+
+    # PCA
+    X_kpca, _, _ = \
+        kernel_pca(X, X, local_rbf_kernel,
+                   deterministic_orientation=True)
+
+    # Plot
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.add_subplot(111)
+
+    ax.scatter(X_kpca[reds, 0], X_kpca[reds, 1], c="red",
+                s=20, edgecolor='k')
+    ax.scatter(X_kpca[blues, 0], X_kpca[blues, 1], c="blue",
+                s=20, edgecolor='k')
+
+    if lims is not None:
+        ax.set_xlim(lims)
+        ax.set_ylim(lims)
+    ax.set_title("Projection by KPCA (gamma={})".format(gamma))
+    ax.set_xlabel(r"1st principal component in space induced by $\phi$")
+    ax.set_ylabel("2nd component")
+    plt.show()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
