471aebc3c4bc839dc89ff22d55398caf93636bcb
José Antonio Álvarez <joseantonioao32@gmail.com>
2022-03-04 02:07:14 +0100

    Final exam started

--- tests/part 1/jose/stochastic_plots.py
+++ tests/part 1/jose/stochastic_plots.py
@@ -10,55 +10,53 @@ Plotting utilities for the trajectories of stochastic processes
 import numpy as np
 import matplotlib.pyplot as plt
 
+fig_size = (8, 6)
 
-def plot_trajectories(t, X, 
-                      max_trajectories=20, 
-                      fig_num=1, fig_size=(8, 4), 
-                      fontsize=14, mean_color='k'):
-    """ Plots a sample of trajectories and their mean """
+
+def plot_trajectories(
+    t, X, max_trajectories=20, fig_num=1, fontsize=14, mean_color="k"
+):
+    """Plots a sample of trajectories and their mean"""
 
     M, _ = np.shape(X)
-    
-    # Plot trajectories 
+
+    # Plot trajectories
     M = np.min((M, max_trajectories))
-    fig, ax = plt.subplots(1, 1, num=fig_num)
+    fig, ax = plt.subplots(1, 1, num=fig_num, figsize=fig_size)
     ax.plot(t, X[:M, :].T, linewidth=1)
-    ax.set_xlabel('t', fontsize=fontsize)
-    ax.set_ylabel('X(t)', fontsize=fontsize)
-    ax.set_title('Simulation', fontsize=fontsize)
-  
+    ax.set_xlabel("t", fontsize=fontsize)
+    ax.set_ylabel("X(t)", fontsize=fontsize)
+    ax.set_title("Simulation", fontsize=fontsize)
+
     # Plot mean
     ax.plot(t, np.mean(X, axis=0), linewidth=3, color=mean_color)
-  
+
     return fig, ax
-  
-    
-def plot_pdf(X, pdf,
-             max_bins=50,
-             fig_num=1, fig_size=(4,4), fontsize=14):
-    """ Compare pdf with the normalized histogram.
-        The normalized histogram is an empirical estimate of the pdf.
+
+
+def plot_pdf(X, pdf, max_bins=50, fig_num=1, fig_size=fig_size, fontsize=14):
+    """Compare pdf with the normalized histogram.
+    The normalized histogram is an empirical estimate of the pdf.
     """
 
     # Plot histogram
-
-    fig, ax = plt.subplots(1, 1, num=fig_num)
-
+    fig, ax = plt.subplots(1, 1, num=fig_num, figsize=fig_size)
     n_samples = len(X)
+    n_bins = np.min((np.int(np.sqrt(n_samples)), max_bins))
 
-    n_bins = np.min((np.int(np.sqrt(n_samples)), 
-                    max_bins))
-       
     ax.hist(X, bins=n_bins, density=True)
-    ax.set_xlabel('x', fontsize=fontsize)
-    ax.set_ylabel('pdf(x)', fontsize=fontsize)
-    
+    ax.set_xlabel("x", fontsize=fontsize)
+    ax.set_ylabel("pdf(x)", fontsize=fontsize)
+
     # Compare with exact distribution
     n_plot = 1000
     x_plot = np.linspace(np.min(X), np.max(X), n_plot)
     y_plot = pdf(x_plot)
-    ax.plot(x_plot, y_plot, linewidth=2, color='r')
-    
+    ax.plot(
+        x_plot,
+        y_plot,
+        linewidth=2,
+        color="r",
+    )
+
     return fig, ax
-
-
